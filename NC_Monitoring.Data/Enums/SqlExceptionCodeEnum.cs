392f202865e062cba6fbafe1cf2b4645b35f2408
Dominik Vesely <dominik.vesely126@gmail.com>
2019-01-13 19:27:37 +0100

    pridani UIException a rozsireni pro vyjimky

---
+++ NC_Monitoring.Data/Enums/SqlExceptionCodeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NC_Monitoring.Data.Enums
+{
+    public enum SqlExceptionCodeEnum
+    {
+        DeleteConstraint            = 547,
+        UniqueConstraint            = 2627,
+        UniqueIndex                 = 2601,
+    }
+}
---
+++ NC_Monitoring.Data/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,59 @@
+﻿using NC_Monitoring.Data.Enums;
+using NC_Monitoring.Data.Exceptions;
+using System;
+using System.Data.SqlClient;
+
+namespace NC_Monitoring.Data.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public static string UIMessage(this Exception ex)
+        {
+            Exception tmp;
+
+            if (TryGetClosestExceptionOfType<UIException>(ex, out tmp))
+            {
+                return ex.Message;
+            }
+            else if (TryGetClosestExceptionOfType<SqlException>(ex, out tmp))
+            {
+                switch ((SqlExceptionCodeEnum)(tmp as SqlException).Number)
+                {
+                    case SqlExceptionCodeEnum.DeleteConstraint:
+                        return "You have to delete all constraints before delete that item";
+                    case SqlExceptionCodeEnum.UniqueConstraint:
+                        return "Duplicate record.";
+                }
+            }
+
+            return "Unknown error";
+        }
+
+
+        private static bool TryGetClosestExceptionOfType<T>(Exception ex, out Exception foundException) where T : Exception
+        {
+            foundException = GetClosestExceptionOfType<T>(ex);
+
+            return foundException != null;
+        }
+
+        private static bool TryGetClosestExceptionOfType<T>(Exception ex, out T foundException) where T : Exception
+        {
+            foundException = GetClosestExceptionOfType<T>(ex);
+
+            return foundException != null;
+        }
+
+        private static T GetClosestExceptionOfType<T>(Exception ex) where T : Exception
+        {
+            Exception tmp = ex;
+
+            while (tmp != null && !(tmp is T))
+            {
+                tmp = tmp.InnerException;
+            }
+
+            return tmp as T;
+        }        
+    }
+}
