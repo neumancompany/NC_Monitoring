3c737ec7e65617ec86f26260eef0369da2c31648
Dominik Vesely <vesely@ncompany.cz>
2019-07-17 20:40:54 +0200

    feat: add propertis to MonitorDetailDTO

--- src/NC_Monitoring.Business/DTO/MonitorDetailDTO.cs
+++ src/NC_Monitoring.Business/DTO/MonitorDetailDTO.cs
@@ -12,20 +12,25 @@ namespace NC_Monitoring.Business.DTO
 
         public string Name { get; set; }
 
+        public int ScenarioId { get; set; }
+
+        public string ScenarioName { get; set; }
+
+        public int StatusId { get; set; }
         public string StatusName { get; set; }
 
+        public bool Enabled { get; set; }
+
+        public int MethodTypeId { get; set; }
         public string MethodTypeName { get; set; }
 
         public string Url { get; set; }
 
+        public int VerificationTypeId { get; set; }
         public string VerificationTypeName { get; set; }
 
         public string VerificationValue { get; set; }
 
         public TimeSpan Timeout { get; set; }
-
-        public int ScenarioId { get; set; }
-
-        public string ScenarioName { get; set; }
     }
 }
